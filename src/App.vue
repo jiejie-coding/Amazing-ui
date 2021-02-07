2070274dd8cc427996df9ededc1b726575d5243d
jiejie <189425098@qq.com>
2021-02-07 19:50:21 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ export default {
     provide("asideVisible", asideVisible);
     router.afterEach(() => {
       if (width < 550) {
+        console.log(1111);
         asideVisible.value = false;
       }
     });
