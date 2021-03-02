37c299b5f93cc300be6143011dda41dad7388d4e
jiejie-coding <189425098@qq.com>
2021-03-02 16:22:28 +0800

    检测Tabs组件内部是否为Tab

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Tabs组件</div>
+  <slot></slot>
+  <!-- <div>Tabs组件</div> -->
+  <component :is="defaults[0]"/>
+  <component :is="defaults[1]"/>
+
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue';
+export default {
+  setup(props,context) {
+    
+    const defaults = context.slots.default();
+    defaults.forEach(tag => {
+      if(tag.type !== Tab) {
+        throw new Error("Tabs 的子组件必须是Tab");
+      }
+    })
+
+    return { defaults }
+  }
+}
+</script>
