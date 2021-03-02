cd6c31d7748e4d8633df6dcffac0fe31db112307
jiejie-coding <189425098@qq.com>
2021-03-02 20:07:38 +0800

    为Tabs标签添加样式，以及实现切换功能

--- src/components/TabsPage.vue
+++ src/components/TabsPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">第一个</Tab>
     <Tab title="导航2">第二个</Tab>
   </Tabs>
@@ -9,10 +9,12 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
+import { ref } from 'vue'
 export default {
   components: { Tabs, Tab },
   setup() {
-
+    const x = ref('导航1')
+    return { x }
   }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,29 @@
 <template>
 
-  <div v-for="(title,index) in titles" :key="index">{{title}}</div>
-  <component v-for="(content, index) in defaults" :is="content" :key="index"/>
-
+  <div class="Amazing-tabs">
+    <div class="Amazing-tabs-nav">
+      <div class="Amazing-tabs-nav-item" 
+        v-for="(title,index) in titles" 
+        :key="index" :class="{'selected': title===selected}"
+        @click="toggle(title)">{{title}}</div>
+        <div class="Amazing-tabs-nav-underline"></div>
+    </div>
+    <div class="Amazing-tabs-content">
+      <component class="Amazing-tabs-content-item" v-for="(content,index) in defaults" :is="content" :key="index" 
+        :class="{'selected': content.props.title === selected}"/>
+    </div>
+  </div>
 </template>
+
 
 <script lang="ts">
 import Tab from './Tab.vue';
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props,context) {
     
     const defaults = context.slots.default();
@@ -19,7 +35,56 @@ export default {
     const titles = defaults.map(tag => {
       return tag.props.title;
     })
-    return { defaults,titles }
+    
+    let toggle = function(title) {
+      context.emit('update:selected',title);
+    }
+    return { defaults,titles,toggle }
   }
 }
 </script>
+
+
+
+<style lang="scss">
+@import './engineer.scss';
+$color: #333;
+$border-color: #d9d9d9;
+.Amazing-tabs {
+  &-nav {
+    position: relative;
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $--color-selected;
+      }
+    }
+    &-underline {
+      position: absolute;
+      width: 100px;
+      left: 0;
+      bottom: 0;
+      height: 3px;
+      background-color: red;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    text-align: left;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
