c7c1d70cffa088660c45d5aa2c3859ae60a784b1
jiejie-coding <189425098@qq.com>
2021-03-03 11:22:43 +0800

    实现对话框的显示与隐藏

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -1,12 +1,23 @@
 <template>
   <h1>实例1</h1>
-  <Dialog/>
+  <Button @click="toggle">点击弹出对话框</Button>
+  <Dialog v-model:visible="x"/>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import { ref } from 'vue';
+
 export default {
-  components: {Dialog}
+  components: {Dialog,Button},
+  setup() {
+    const x = ref(false);
+    const toggle = ()=> {
+      x.value = !x.value;
+    }
+    return { x,toggle }
+  }
 }
 </script>
 
