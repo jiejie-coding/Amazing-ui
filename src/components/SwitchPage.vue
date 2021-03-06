15ca08eacc66ecf827e2403425ce5b0110fe2caa
jiejie-coding <189425098@qq.com>
2021-03-06 10:37:52 +0800

    解释说明Swtich组件的属性

--- src/components/SwitchPage.vue
+++ src/components/SwitchPage.vue
@@ -4,6 +4,7 @@
     <p>适用条件：想要表示两种状态之间的切换可以用到该组件 [on/off]</p>
     <Demo :component="SwitchDemo1" />
     <Demo :component="SwitchDemo2" />
+    <Attribute :data="data"/>
   </div>
 </template>
 
@@ -13,19 +14,37 @@ import "prismjs";
 import "prismjs/themes/prism.css";
 import SwitchDemo1 from "../user/SwitchDemo1.vue";
 import SwitchDemo2 from "../user/SwitchDemo2.vue";
+import Attribute from '../components/Attribute.vue';
 import Demo from './Demo.vue';
 
 const Prism = (window as any).Prism;
 
 export default {
-  components: { SwitchDemo1, SwitchDemo2, Demo },
+  components: { SwitchDemo1, SwitchDemo2, Demo, Attribute },
   setup() {
     const bool = ref(false);
+    const data = [
+      {
+        params: "value",
+        desc: "值",
+        type: "Boolean",
+        select: "true / false",
+        default: "false",
+      },
+      {
+        params: "disabled",
+        desc: "是否禁用",
+        type: "String",
+        select: " disabled / 不传",
+        default: " 不传 ",
+      }
+    ];
     return {
       bool,
       SwitchDemo1,
       SwitchDemo2,
       Prism,
+      data
     };
   },
 };
