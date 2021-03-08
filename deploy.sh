63c5a05526ce199620b8f9f580e87a15bfd73b55
jiejie-coding <189425098@qq.com>
2021-03-08 18:58:27 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git init
 git add .
 git commit -m "一键部署"
 git branch -M master
-git remote add origin git@github.com:jiejie-coding/AmazingUI.git
-git push -f -u origin master
+git push -f git@github.com:jiejie-coding/Amazing-ui.git master:gh-pages
+
 cd -
