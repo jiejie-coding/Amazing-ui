3c571f90e52fd5bf972615c6e1f353a1633c6af1
jiejie-coding <189425098@qq.com>
2021-03-08 10:57:13 +0800

    修改首页布局

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,49 +1,61 @@
 <template>
   <div>
-    <top-nav prefix="home"/>
+    <top-nav prefix="home" />
     <div class="banner">
       <h1>
-        <img src="../assets/imgs/logo.png"/>
+        <img src="../assets/imgs/logo.png" />
       </h1>
-      <div class="actions">
-        <router-link to="/home">home</router-link>
-        <a href="https://github.com/jiejie-coding/Amazing-ui">Github</a>
-        <router-link to="/doc">start</router-link>
-      </div>
+      <ul class="actions">
+        <li class="actions-item actions-checked">
+          <router-link to="/home">home</router-link>
+        </li>
+        <li class="actions-item">
+          <a href="https://github.com/jiejie-coding/Amazing-ui">Github</a>
+        </li>
+        <li class="actions-item">
+          <router-link to="/doc">start</router-link>
+        </li>
+      </ul>
     </div>
     <div>
       <ul class="features">
         <li>
-          
-          <span class="features-text">feature: one</span>
-           <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-shejishi"></use>
-            </svg>
-            <span class="features-text">Excellent UI design</span>
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-shejishi"></use>
+          </svg>
+          <div class="features-text">
+            <p class="features-text-title">UI设计</p>
+            <p class="features-text-main">融合了许多优秀的UI设计</p>
+          </div>
         </li>
         <li>
-          
-          <span class="features-text">feature: two</span>
           <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-Vue"></use>
-            </svg>
-            <span class="features-text">A Vue Component Library</span>
+            <use xlink:href="#icon-Vue"></use>
+          </svg>
+          <div class="features-text">
+            <p class="features-text-title">基于Vue3</p>
+            <p class="features-text-main">使用了许多Vue3的新特性</p>
+          </div>
         </li>
         <li>
-          
-          <span class="features-text">feature: three</span>
           <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-typescript"></use>
-            </svg>
-            <span class="features-text">Based on typescript</span>
+            <use xlink:href="#icon-typescript"></use>
+          </svg>
+          <div class="features-text">
+            <p class="features-text-title">基于Typescript</p>
+            <p class="features-text-main">全程使用Typescript编写</p>
+          </div>
         </li>
         <li>
-          <span class="features-text">feature: four</span>
+          <!-- <span class="features-text">feature: four</span> -->
           <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-ai-code"></use>
-            </svg>
-            
-            <span class="features-text">Simple code</span>
+            <use xlink:href="#icon-ai-code"></use>
+          </svg>
+
+          <div class="features-text">
+            <p class="features-text-title">代码简洁</p>
+            <p class="features-text-main">每个组件的源代码简洁易读</p>
+          </div>
         </li>
       </ul>
     </div>
@@ -51,10 +63,10 @@
 </template>
 
 <script lang='ts'>
-import TopNav from '../components/TopNav.vue';
+import TopNav from "../components/TopNav.vue";
 export default {
-  components: {TopNav}
-}
+  components: { TopNav },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -70,62 +82,74 @@ export default {
   > h3 {
     margin: 15px 0;
   }
-  > .actions{
-    padding: 50px 0;
+  .actions {
+    padding: 10px 0 30px;
+    display: flex;
     font: 16px Arial, Helvetica, sans-serif;
-    font-family: 'Teko', sans-serif;
+    font-family: "Teko", sans-serif;
     text-transform: uppercase;
-    letter-spacing: .2px;
-    & :first-child {
-      color: #666;
-      background: #fff;
-    
-    }
-    a {
-      color: #fff;
-      display: inline-block;
-      margin:0 20px;
-      padding: 10px 15px;
-      transition: all .2s;
-      &:hover {
-        background-color: #fff;
+    letter-spacing: 0.2px;
+    &-checked.actions-item {
+      background-color: #fff;
+      > a {
         color: #131313;
-        
+        &:hover {
+          color: fade-out($color: #131313, $amount: 0.3);
+        }
       }
     }
-    
+    &-item {
+      position: relative;
+      padding: 10px 20px;
+      margin: 15px;
+      z-index: 1;
+      > a {
+        color: #fff;
+      }
+      &::after {
+        position: absolute;
+        content: "";
+        display: block;
+        width: 100%;
+        height: 0px;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        background-color: #fff;
+        transition: all 250ms;
+      }
+      &:hover {
+        > a {
+          color: #131313;
+        }
+        &::after {
+          height: 100%;
+        }
+      }
+    }
   }
 }
 .features {
   height: 200px;
   display: flex;
-  justify-content: center;
   flex-wrap: wrap;
+  justify-content: center;
   > li {
-    display: flex;
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    padding: 20px 0;
-    width: 330px;
-    display: flex;
-    align-items: center;
-    border: 1px dashed #ccc;
-    margin: 10px;
-    > .features-text {
-      text-align: left;
-      margin: 20px;
-      font: 16px Arial, Helvetica, sans-serif;
-      font-family: 'Teko', sans-serif;
-      text-transform: uppercase;
-      letter-spacing: .2px;
-      
+    padding: 20px;
+    .features-text {
+      color: #131313;
+      &-title {
+        font-size: 20px;
+        margin: 10px;
+      }
+      &-main {
+        font-size: 16px;
+      }
     }
-  }
-
-  .icon {
-    width: 68px;
-    height: 68px;
+    > .icon {
+      width: 68px;
+      height: 68px;
+    }
   }
 }
 </style> 	
